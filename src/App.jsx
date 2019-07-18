d1acaec301cda7bd42ff4a57507876a47a487f80
delmontz <delmontz@dgl.tokyo>
2019-07-19 01:28:35 +0900

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+   const [count, setCount] = useState(0);
+
+   const increment = () => setCount(count + 1);
+   const decrement = () => setCount(count - 1);
+   const increment2 = () => setCount(previousCount => previousCount + 1);
+   const decrement2 = () => setCount(hoge => {
+      return hoge - 1;
+   });
+   const reset = () => setCount(0);
+
    return (
-      <div>
-         <h1>hogehoee</h1>
-      </div>
+      <> 
+         <div>カウント: { count }</div>
+         <div>
+            <button onClick={ increment }>＋１</button>
+            <button onClick={ decrement }>ー１</button>
+         </div>
+         <div>
+            <button onClick={ increment2 }>＋１</button>
+            <button onClick={ decrement2 }>ー１</button>
+         </div>
+         <div>
+            <button onClick={ reset }>Reset</button>
+            <button onClick={ reset }>Reset</button>
+         </div>
+      </>
    );
 }
 
