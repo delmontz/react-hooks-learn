429671423b508d7e618d2de6c52a975dcab5e704
delmontz <delmontz@dgl.tokyo>
2019-07-29 00:03:18 +0900

    イベント削除とボタンバリデートを実装

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,6 +24,14 @@ const App = () => {
       setBody('');
    };
 
+   const deleteAllEvents = e => {
+      e.preventDefault();
+      const result = window.confirm('全てのイベントを削除してもよいかのぉ？');
+      if(result)dispatch({ type: 'DELETE_ALL_EVENTS' });
+   }
+
+   const unCreatable =  title === '' || body === '';
+
    return (
       <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -38,8 +46,8 @@ const App = () => {
             <textarea className="form-control" id="formEventBody" value={ body } onChange={ e => setBody(e.target.value) }/>
          </div>
 
-         <button className="btn btn-primary" onClick={ addEvent }>イベントを作成する</button>
-         <button className="btn btn-danger">全てのイベントを削除する</button>
+         <button className="btn btn-primary" disabled={ unCreatable } onClick={ addEvent }>イベントを作成する</button>
+         <button className="btn btn-danger" disabled={ state.length === 0 } onClick={ deleteAllEvents }>全てのイベントを削除する</button>
       </form>
 
       <h4>イベント一覧</h4>
--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const  Event = ({event, dispatch}) => {
    const id = event.id;
    const handleClickDeleteButton = () => {
-      dispatch({
+      const result = window.confirm(`イベントID:${id}を削除してもよいかのぉ？`);
+      if(result)dispatch({
          type: 'DELETE_EVENT',
          id
       });
